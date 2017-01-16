c07e23b418279991c4a1e8a1f84c988f23d9c9d4
soorajparemal <srjps202@gmail.com>
2017-01-16 19:26:59 +0530

    polls_app_new

---
+++ mysite/quiz/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from .models import Question
+# Create your views here.
+
+
+def Index(request):
+    latest_questions= Question.objects.order_by("-pub_date")[:5]
+    context={'latest_questions':latest_questions}
+    return render(request, 'quiz/index.html', context)
+
+def detail(request, question_id):
+    question=get_object_or_404(Question, pk =question_id)
+    return render(request, 'quiz/detail.html',{'question':question})
+
+def results(request,question_id):
+    question= get_object_or_404(Question,pk=question_id)
+    return render(request,'quiz/results.html', {'question':question})
+
+def vote(request,question_id):
+    question=get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST['choice'])
+    except:
+        return render(request, 'quiz/detail.html', {'question':question, 'error_message':'Please select a choice'})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('quiz:results',args=(question.id,)))
+
+
